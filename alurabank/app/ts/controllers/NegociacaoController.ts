734efd26d7fef6f2e0cfcb44c092bab940fb53f4
Mário Santos <super.mario.santos.sousa@gmail.com>
2018-12-25 14:42:45 -0300

    aula 3 typescript 2

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,19 +1,20 @@
 import { NegociacoesView, MensagemView } from '../views/index';
 import { Negociacoes, Negociacao } from '../models/index';
+import { domInject } from '../helpers/decorators/index';
 
 export class NegociacaoController{
     
+    @domInject('#data')
     private _inputData: JQuery;
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+    @domInject('#valor')
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView("#mensagemView");
 
     constructor(){
-        this._inputData = $("#data");
-        this._inputQuantidade = $("#quantidade");
-        this._inputValor = $("#valor");
         this._negociacoesView.update(this._negociacoes);
     }
 
@@ -24,7 +25,7 @@ export class NegociacaoController{
 
         if(!this.ehDiaUtil(data)){
             this._mensagemView.update('Somente negociação em dias úteis..');
-            return
+            return;
         }
 
         const negociacao = new Negociacao(
@@ -36,7 +37,8 @@ export class NegociacaoController{
         this._negociacoes.adiciona(negociacao);
         this._negociacoesView.update(this._negociacoes);
 
-        this._mensagemView.update('Negociação adicionada com sucesso!');
+        this._mensagemView.update('Negociação adicionada com sucesso!');      
+        
     }
 
     private ehDiaUtil(data : Date) : boolean{
--- alurabank/app/ts/views/View.ts
+++ alurabank/app/ts/views/View.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExcucao } from '../helpers/decorators/index';
+
 export abstract class View<T>{
 
     protected elemento : JQuery;
@@ -8,6 +10,7 @@ export abstract class View<T>{
         this.escapar = escapar;
     }
 
+    @logarTempoDeExcucao(true)
     update(model:T) : void{
         let template = this.template(model);
         if(this.escapar){
