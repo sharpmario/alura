9bef616acd48500c459e0a1938003369fe7160f2
Mário Santos <super.mario.santos.sousa@gmail.com>
2018-12-31 15:53:20 -0300

    aula 4 javascript

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,5 +1,16 @@
 var pacientes = document.querySelectorAll(".paciente");
 
+function calculaImc(peso, altura){
+    return peso / (altura * altura);
+}
+
+function criaTd(content, cssClass){
+    var td = document.createElement("td")
+    td.textContent = content;
+    td.classList.add(cssClass);
+    return td;
+}
+
 for(var i = 0; i<pacientes.length; i++){
 
     var paciente = pacientes[i];
@@ -19,9 +30,47 @@ for(var i = 0; i<pacientes.length; i++){
     }
 
     if(!pesoInvalido){
-        var imc = peso / (altura * altura);
+        var imc = calculaImc(peso, altura);
         console.log(imc);
         paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
     }
 
 }
+
+var btnAdd = document.querySelector('#adicionar-paciente');
+btnAdd.addEventListener("click",function(event){
+    event.preventDefault();
+
+    var form = document.querySelector('#form-adiciona');
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var gordura = form.gordura.value;
+    var altura = form.altura.value;
+
+    var tr = document.createElement("tr");
+
+    tr.appendChild(
+        criaTd(nome, 'info-nome')
+    );
+
+    tr.appendChild(
+        criaTd(peso, 'info-peso')
+    );
+
+    tr.appendChild(
+        criaTd(altura, 'info-altura')
+    );
+
+    tr.appendChild(
+        criaTd(gordura, 'info-gordura')
+    );
+
+    var imc = calculaImc(peso,altura);
+    tr.appendChild(
+        criaTd(imc.toFixed(2),'info-imc')
+    );
+
+    var tbody = document.querySelector('#tabela-pacientes');
+    tbody.appendChild(tr);
+});
+
