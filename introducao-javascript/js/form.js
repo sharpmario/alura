def63e4b0a431161cc0ba1830e7fdda35b9aabd1
Mario <super.mario.santos.sousa@gmail.com>
2019-01-07 18:03:14 -0300

    aula 6 js intro

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -3,8 +3,23 @@ btnAdd.addEventListener("click",function(event){
     event.preventDefault();
     let form = document.querySelector('#form-adiciona');
     let paciente = obtemPacienteDoFormulario(form);
-    montaTr(paciente);
+    let tr = montaTr(paciente);
+
+    console.log(paciente);
+
+    let erros = validaPaciente(paciente);
+    console.log(erros);
+    
+    if(erros.length > 0){
+
+        exibeMensagemDeErros(erros);
+
+        return;
+    }
+   
+    document.querySelector('#tabela-pacientes').appendChild(tr);
     form.reset();
+    limpaMensagensErro();
 });
 
 function obtemPacienteDoFormulario(form){
@@ -29,7 +44,7 @@ function montaTr(paciente){
     tr.appendChild(montaTd(paciente.gordura, 'info-gordura'));
     tr.appendChild(montaTd(paciente.imc,'info-imc'));
 
-    document.querySelector('#tabela-pacientes').appendChild(tr);
+    return tr;
 }
 
 function montaTd(content, cssClass){
@@ -37,4 +52,46 @@ function montaTd(content, cssClass){
     td.textContent = content;
     td.classList.add(cssClass);
     return td;
+}
+
+function validaPaciente(paciente){
+    let erros = [];
+    
+    if(paciente.nome.length == 0){
+        erros.push("Informe o nome do paciente!");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("Informe a altura do paciente!");
+    }else if(!validaAltura(paciente.altura)){
+        erros.push("Altura inválida!");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("Informe a gordura do paciente!");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("Informe o peso do paciente!");
+    }else if(!validaPeso(paciente.peso)){
+        erros.push("Peso inválido!")
+    }
+
+    return erros;
+}
+
+function exibeMensagemDeErros(erros){
+    limpaMensagensErro();
+    let ul = document.querySelector('#mensagens-erro');
+
+    erros.forEach(msg => {
+        let li = document.createElement("li");
+        li.textContent = msg;
+        ul.appendChild(li);
+    });
+    
+}
+
+function limpaMensagensErro(){
+    document.querySelector('#mensagens-erro').innerHTML = "";
 }
--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -14,23 +14,45 @@ function atualizaTabela(){
         var peso = paciente.querySelector(".info-peso").textContent;
         var altura = paciente.querySelector(".info-altura").textContent;
         var gordura = paciente.querySelector(".info-gordura").textContent;
-        var pesoInvalido = false;
+        var pesoInvalido = !validaPeso(peso);
+        var alturaInvalida = !validaAltura(altura);
 
         console.log(peso, altura, gordura);
 
-        if(peso <= 0 || peso >= 300){
+        if(pesoInvalido){
             paciente.querySelector(".info-imc").textContent = "Peso inválido!"
-            pesoInvalido = true;
             paciente.classList.add('paciente-invalido');
         }
 
-        if(!pesoInvalido){
+        if(alturaInvalida){
+            paciente.querySelector(".info-imc").textContent = "Altura inválida!"
+            paciente.classList.add('paciente-invalido');
+        }
+
+        if(!pesoInvalido && !alturaInvalida){
             var imc = calculaImc(peso, altura);
             console.log(imc);
             paciente.querySelector(".info-imc").textContent = imc;
         }
 
     }
+}
+
+function validaPeso(peso){
+    if(peso <= 0 || peso >= 300){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function validaAltura(altura){
+    if(altura <=0 || altura >= 3.00){
+        return false;
+    }else{
+        return true;
+    }
+
 }
 
 atualizaTabela();
