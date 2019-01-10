d2bbafa46afd76832cc764e6f4bc60e99f60d906
Mário Santos <super.mario.santos.sousa@gmail.com>
2019-01-09 21:06:32 -0300

    aula 1 angular 1

---
+++ alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Alurapic';
+  description ='Leao';
+  url='http://www.chumbogordo.com.br/wp-content/uploads/2018/11/leao.jpg';
+}
