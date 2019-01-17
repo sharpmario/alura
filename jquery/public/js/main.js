3077081064a5f42f0f334dabce478f985df143b9
Mario <super.mario.santos.sousa@gmail.com>
2019-01-17 08:55:59 -0300

    aula 2 jquery 1

--- jquery/public/js/main.js
+++ jquery/public/js/main.js
@@ -2,3 +2,13 @@ var frase = $('.frase').text();
 var numeroPalavras = frase.split(" ").length;
 $("#tamanho-frase").text(numeroPalavras);
 
+let campo = $(".campo-digitacao");
+campo.on("input", function(event){
+    let conteudo = campo.val();
+    let qtdPalavras = conteudo.split(/\S+/).length -1;
+    let qtdCaracteres = conteudo.length;
+
+    $("#contador-caracteres").text(qtdCaracteres);
+    $("#contador-palavras").text(qtdPalavras);
+    
+});
