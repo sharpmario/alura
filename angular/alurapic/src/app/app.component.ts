7a5542cdd9320af72938ca9b6aaf810210fb4874
Mario <super.mario.santos.sousa@gmail.com>
2019-01-21 18:12:50 -0300

    iniciando aula 4 angular 1

--- angular/alurapic/src/app/app.component.ts
+++ angular/alurapic/src/app/app.component.ts
@@ -1,25 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { PhotoService } from './photos/photo/photo.service';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   
-  title = 'Alurapic';
-  photos : Object[] = [];
-  
-  constructor(private photoService : PhotoService){}
-  
-  ngOnInit():void{
-    this.photoService
-      .listFromUser( 'flavio')
-      .subscribe(photos => {
-        this.photos = photos
-      });
-  }
+
 }
