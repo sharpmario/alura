90f303501c6b942a76f72185752ceb45d9fec575
Mario <super.mario.santos.sousa@gmail.com>
2019-01-23 18:05:34 -0300

    aula 4 angular

--- angular/alurapic/src/app/app.module.ts
+++ angular/alurapic/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/alurapic/src/app/photos/photos.module.ts
+++ angular/alurapic/src/app/photos/photos.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
 
 import { PhotoComponent } from "./photo/photo.component";
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-    declarations:[PhotoComponent, PhotoListComponent],
-    imports:[HttpClientModule]
+    declarations:[PhotoComponent, PhotoListComponent, PhotoFormComponent],
+    imports:[
+        HttpClientModule,
+        CommonModule
+    ]
 })
 export class PhotosModule{}
