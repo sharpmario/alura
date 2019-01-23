c5676454b7e162bf06aa6eee7594bc76f1150d73
Mário Santos <super.mario.santos.sousa@gmail.com>
2019-01-23 20:56:49 -0300

    concluindo aula 4 angular 1

--- angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -11,11 +13,14 @@ export class PhotoListComponent implements OnInit {
   title = 'Alurapic';
   photos : Object[] = [];
   
-  constructor(private photoService : PhotoService){}
+  constructor(
+    private photoService : PhotoService,
+    private activatedRoute: ActivatedRoute){}
   
   ngOnInit():void{
+    const userName = this.activatedRoute.snapshot.params.userName;
     this.photoService
-      .listFromUser( 'flavio')
+      .listFromUser( userName )
       .subscribe(photos => {
         this.photos = photos
       });
--- angular/alurapic/src/app/photos/photo/photo.service.ts
+++ angular/alurapic/src/app/photos/photo/photo.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./photo";
 
+const API = 'http://localhost:3000';
+
 @Injectable({providedIn:'root'})
 export class PhotoService{
 
@@ -9,6 +11,6 @@ export class PhotoService{
 
     listFromUser(userName : string){
         return this.http
-          .get<Photo[]>('http://localhost:3000/flavio/photos');
+          .get<Photo[]>(API+'/'+userName+'/photos');
     }
 }
