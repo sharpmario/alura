59ad8b319eda8c0238f6bc05703f79c8aedf11f0
Mário Santos <super.mario.santos.sousa@gmail.com>
2019-01-24 08:50:30 -0300

    aula 4 jquery 1

--- jquery/public/js/main.js
+++ jquery/public/js/main.js
@@ -1,27 +1,58 @@
-var frase = $('.frase').text();
-var numeroPalavras = frase.split(" ").length;
-$("#tamanho-frase").text(numeroPalavras);
 
-let campo = $(".campo-digitacao");
-campo.on("input", function(event){
-    let conteudo = campo.val();
-    let qtdPalavras = conteudo.split(/\S+/).length -1;
-    let qtdCaracteres = conteudo.length;
+var contadorCaracteres = $("#contador-caracteres");
+var contadorPalavras = $("#contador-palavras");
+var tempoInicial = $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao");
 
-    $("#contador-caracteres").text(qtdCaracteres);
-    $("#contador-palavras").text(qtdPalavras);
+function atualizaTamanhoFase(){
+    var frase = $('.frase').text();
+    var numeroPalavras = frase.split(" ").length;
+    $("#tamanho-frase").text(numeroPalavras);
+}
+
+function inicializaContadores(){
+
+    campo.on("input", function(event){
+        let conteudo = campo.val();
+        let qtdPalavras = conteudo.split(/\S+/).length -1;
+        let qtdCaracteres = conteudo.length;
     
+        contadorCaracteres.text(qtdCaracteres);
+        contadorPalavras.text(qtdPalavras);
+        
+    });
+}
+
+function inicializaCronometro(){
+    let tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function(){
+        $("#botao-reiniciar").attr("disabled",true);
+        let id = setInterval(function(){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if(tempoRestante==0){
+                campo.attr("disabled",true);
+                clearInterval(id);
+                $("#botao-reiniciar").attr("disabled", false);
+            }
+        },1000);
+    });
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val('');
+    contadorCaracteres.text("0");
+    contadorPalavras.text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
+
+$(document).ready(function(){
+    atualizaTamanhoFase();
+    inicializaContadores();
+    inicializaCronometro();
+
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-
-campo.one("focus", function(){
-    var id = setInterval(function(){
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if(tempoRestante==0){
-            campo.attr("disabled",true);
-            clearInterval(id);
-        }
-    },1000);
-});
