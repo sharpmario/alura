7984bdf16fa9f1a20ba0a469db525a29b3a39242
Mário Santos <super.mario.santos.sousa@gmail.com>
2019-01-25 23:15:46 -0300

    aula 5 angular 1

--- angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
 
   title = 'Alurapic';
-  photos : Object[] = [];
+  photos : Photo[] = [];
   
   constructor(
     private photoService : PhotoService,
