7c128d78a09233de253fcedb66f5c4135db582fb
Mário Santos <super.mario.santos.sousa@gmail.com>
2019-02-09 17:33:36 -0300

    aula 7 angular 1

--- angular/alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ angular/alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -5,17 +5,23 @@ import { PhotosComponent } from "./photos/photos.component";
 import { FilterByDescrition } from "./filter-by-description.pipe";
 import { CommonModule } from "@angular/common";
 import { PhotoModule } from "../photo/photo.module";
+import { CardModule } from "src/app/shared/components/card/card.module";
+import { SearchComponent } from "./search/search.component";
+import { DarkenOnHoverModule } from "src/app/shared/directives/darken-on-hover/darken-on-hover.module";
 
 @NgModule({
     declarations:[
         PhotoListComponent,
         PhotosComponent,
         LoadButtonComponent,
-        FilterByDescrition
+        FilterByDescrition, 
+        SearchComponent
     ],
     imports:[
         CommonModule,
-        PhotoModule
+        PhotoModule,
+        CardModule,
+        DarkenOnHoverModule
     ]
 })
 export class PhotoListModule{
---
+++ angular/alurapic/src/app/photos/photo-list/search/search.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnDestroy, OnInit, Output, EventEmitter, Input } from "@angular/core";
+import { Subject } from "rxjs";
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+    selector:'ap-search',
+    templateUrl: './search.component.html'
+})
+export class SearchComponent implements OnDestroy, OnInit{
+
+    @Output() onTyping = new EventEmitter<string>();
+    debounce: Subject<string> = new Subject<string>();
+    @Input() value: string = '';
+    
+    ngOnInit(): void {
+        this.debounce
+            .pipe(debounceTime(300))
+            .subscribe(filter => this.onTyping.emit(filter) );
+    }
+
+    ngOnDestroy(): void {
+        this.debounce.unsubscribe();
+    }
+}
