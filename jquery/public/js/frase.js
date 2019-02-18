dddad685dbf7f7ab5a808f1dcf398e31713a558e
Mario <super.mario.santos.sousa@gmail.com>
2019-02-18 11:27:58 -0300

    aula 3 jquery 2

--- jquery/public/js/frase.js
+++ jquery/public/js/frase.js
@@ -1,13 +1,25 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
+    $("#spinner").show();
+
     $.get("http://localhost:3000/frases",function(data){
+
         console.log(data);
         let numeroAleatorio = Math.floor(Math.random() * data.length);
         let frase = data[numeroAleatorio];
         $(".frase").text(frase.texto);        
         atualizaTempo(frase.tempo);
         atualizaTamanhoFase();
+    }).fail(function(){
+        $("#erro").show();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2000);
+        
+    }).always(function(){
+        $("#spinner").toggle();
     });
-}
 
+
+}
