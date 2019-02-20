f279ed029a9549885a6f31e3e93182592c7a4834
Mário Santos <super.mario.santos.sousa@gmail.com>
2019-02-19 21:43:12 -0300

    aula 4 jquery 2

--- jquery/public/js/frase.js
+++ jquery/public/js/frase.js
@@ -1,16 +1,14 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria(){
     $("#spinner").show();
 
     $.get("http://localhost:3000/frases",function(data){
-
         console.log(data);
         let numeroAleatorio = Math.floor(Math.random() * data.length);
         let frase = data[numeroAleatorio];
-        $(".frase").text(frase.texto);        
-        atualizaTempo(frase.tempo);
-        atualizaTamanhoFase();
+        trocaFrase(frase);
     }).fail(function(){
         $("#erro").show();
         setTimeout(function(){
@@ -22,4 +20,27 @@ function fraseAleatoria(){
     });
 
 
+}
+
+function buscaFrase(){
+    $("#spinner").show();
+    let id = $("#frase-id").val();
+    let dados = {id};
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").show();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2000);
+        
+    }).always(function(){
+        $("#spinner").toggle();
+    });
+
+}
+
+function trocaFrase(frase){    
+    $(".frase").text(frase.texto);        
+    atualizaTempo(frase.tempo);
+    atualizaTamanhoFase();
 }
